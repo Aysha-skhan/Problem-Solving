7e07bd188541c767c9906b2d7a0e4893680081b5
Ayesha khan <136958777+Aysha-skhan@users.noreply.github.com>
2024-12-01 19:31:40 +0500

    Create caps lock.py

---
+++ caps lock.py
@@ -0,0 +1,27 @@
+s=input()
+if len(s)==1:
+    if ord(s[0]) < 97:
+        res = chr(ord(s[0]) + 32)
+    else:
+        res=chr(ord(s[0]) & ~32)
+    print(res)
+else:
+    as_it_is=-1
+    res=''
+    for y in range(len(s)):
+        if y==0:
+            if ord(s[y])<97:
+                res += chr(ord(s[0]) + 32)
+            else:
+                res += chr(ord(s[0]) & ~32)
+
+        else:
+            if ord(s[y]) >= 97:
+                as_it_is+=1
+                break
+            else:
+                res += chr(ord(s[y]) + 32)
+    if as_it_is==0:
+        print(s)
+    else:
+        print(res)
