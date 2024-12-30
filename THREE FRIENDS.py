1a45fc075544d81323635d524e387c426a7f320d
Ayesha khan <136958777+Aysha-skhan@users.noreply.github.com>
2024-12-30 18:09:32 +0500

    Updated

--- THREE FRIENDS.py
+++ THREE FRIENDS.py
@@ -20,6 +20,7 @@ for z in range(t):
                 l[0] = l[0] + 1
                 l[2] = l[2] - 1
             print(abs(l[1]-l[0])+abs(l[2]-l[0])+abs(l[1]-l[2]))
+            # print(abs(l[1]))
     else:
         l = [a, b, c]
         l.sort()
